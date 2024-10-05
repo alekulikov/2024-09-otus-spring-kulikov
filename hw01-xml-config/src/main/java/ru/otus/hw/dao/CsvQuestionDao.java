fb1318715b718c93d0af6a191959e37df3ac7e02
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2024-10-05 15:03:23 +0300

    hw01-xml-config | complete

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,48 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+
+    private static final char SEPARATOR = ';';
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        try (InputStreamReader inputStreamReader = new InputStreamReader(getInputStreamFromResource(
+                fileNameProvider.getTestFileName()))) {
+            CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(inputStreamReader)
+                    .withType(QuestionDto.class)
+                    .withSeparator(SEPARATOR)
+                    .withSkipLines(1)
+                    .build();
+            return csvToBean.parse().stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (IOException e) {
+            throw new QuestionReadException(e.getMessage(), e);
+        }
+    }
 
-        return new ArrayList<>();
+    private InputStream getInputStreamFromResource(String fileName) {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
     }
 }
